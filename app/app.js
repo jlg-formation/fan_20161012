698049daaf1df1ca09e359c505db45794c4eae41
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-10-11 11:09:52 +0200

    ors star

--- app/app.js
+++ app/app.js
@@ -36,4 +36,24 @@
 			transclude: true
 		};
 	});
+	
+	app.directive('orsStar', function() {
+		return {
+			restrict: 'E',
+			link: function(scope, element, attr) {
+				console.log('link', arguments);
+				var html = '';
+				var note = 4;
+				for (var i = 0; i < note; i++) {
+					html += '<img src="img/yellow_star.png" />';
+				}
+				for (var i = note; i < 5; i++) {
+					html += '<img src="img/white_star.png" />';
+				}
+				element.html(html);
+				
+			}
+		};
+	});
+	
 })();
